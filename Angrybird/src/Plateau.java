811852d475a6a984860855dd4ad72a1880630408
Remi DEJONGHE <remi.dejonghe@etudiant.univ-lille1.fr>
2015-10-05 13:30:42 +0200

    Repetition

--- Angrybird/src/Plateau.java
+++ Angrybird/src/Plateau.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
@@ -6,6 +8,7 @@ public class Plateau extends JFrame{
 	private int echelle;
 
 	Bird pan = new Bird(echelle);
+	Random r = new Random();
 	
 	//constructeur
 	public Plateau(){
@@ -16,18 +19,31 @@ public class Plateau extends JFrame{
 	    this.setLocationRelativeTo(null);
 	    this.setContentPane(pan);
 	    this.setVisible(true);
+	    int x=1;
+	    while(x==1){
 	    go();
+	    }
 	}
 	
 	private void go() {
+	
+		pan.setPosY(550);
+		pan.setPosX(-50);
+		
 	    int x = pan.getPosX(), y = pan.getPosY();
 
 	    int t =-30;
 	    
-	    //condition d'arrêt à revoir
+	    int a = (r.nextInt(10))+1;  
+	    int b = r.nextInt(10);
+	    int c = r.nextInt(10);
+	    
+	    
+	    //condition d'arrï¿½t ï¿½ revoir
 	    while (x < 1990) {
 	    	t++;
-	        y = (int) (2 * Math.pow(t, 2) + 6* t +5);
+	        //y = (int) (2 * Math.pow(t, 2) + 6* t +5);
+	    	y = (int) (a* Math.pow(t, 2) + b* t +c);
 	    	x= x+35;
 	        pan.setPosX(x++/echelle);
 	        System.out.println(x+"  "+y);
